db4975fde0d261b8e4b8f2618547e650639abac3
Nahom_zd <51995150+zdnahom@users.noreply.github.com>
2023-01-31 10:53:27 +0300

    Update script.js to add menu functionality

---
+++ script.js
@@ -0,0 +1,18 @@
+function toggleMobileMenu() {
+    document
+      .getElementById('nav-dropdown-menu')
+      .classList.toggle('hidden-dropdown-menu');
+    document
+      .getElementById('intro-section')
+      .classList.toggle('blur-intro-section');
+  }
+  
+  function openDropdownItem(menuElement, menuLink) {
+    document
+      .getElementById('nav-dropdown-menu')
+      .classList.toggle('hidden-dropdown-menu');
+    document
+      .getElementById('intro-section')
+      .classList.toggle('blur-intro-section');
+    menuElement.href = menuLink;
+  }
