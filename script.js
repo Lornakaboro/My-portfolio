dd40a953257eabe07730b4eee434c54269934c51
Lornakaboro <lornakaboro@gmail.com>
2023-01-31 12:55:30 +0300

    Modify mobile-menu function

--- script.js
+++ script.js
@@ -4,17 +4,10 @@ function toggleMobileMenu() {
   document
     .getElementById('nav-dropdown-menu')
     .classList.toggle('hidden-dropdown-menu');
-  document
-    .getElementById('intro-section')
-    .classList.toggle('blur-intro-section');
 }
 
 function openDropdownItem(menuElement, menuLink) {
   document
     .getElementById('nav-dropdown-menu')
     .classList.toggle('hidden-dropdown-menu');
-  document
-    .getElementById('intro-section')
-    .classList.toggle('blur-intro-section');
-  menuElement.href = menuLink;
 }
