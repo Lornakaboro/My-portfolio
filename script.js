378eaf2a8bbd0e0d05a98769ab56486bae761420
Lornakaboro <lornakaboro@gmail.com>
2023-01-31 11:31:23 +0300

    Fix additional linter errors

--- script.js
+++ script.js
@@ -2,19 +2,19 @@
 
 function toggleMobileMenu() {
   document
-    .getElementById("nav-dropdown-menu")
-    .classList.toggle("hidden-dropdown-menu");
+    .getElementById('nav-dropdown-menu')
+    .classList.toggle('hidden-dropdown-menu');
   document
-    .getElementById("intro-section")
-    .classList.toggle("blur-intro-section");
+    .getElementById('intro-section')
+    .classList.toggle('blur-intro-section');
 }
 
 function openDropdownItem(menuElement, menuLink) {
   document
-    .getElementById("nav-dropdown-menu")
-    .classList.toggle("hidden-dropdown-menu");
+    .getElementById('nav-dropdown-menu')
+    .classList.toggle('hidden-dropdown-menu');
   document
-    .getElementById("intro-section")
-    .classList.toggle("blur-intro-section");
+    .getElementById('intro-section')
+    .classList.toggle('blur-intro-section');
   menuElement.href = menuLink;
 }
