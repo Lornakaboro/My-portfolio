69db00915c7fa9f32079dd06eee2e46816b0e0a3
Lornakaboro <lornakaboro@gmail.com>
2023-01-31 16:47:08 +0300

    Add Javascript object for mobile work section

--- script.js
+++ script.js
@@ -11,3 +11,54 @@ function openDropdownItem(menuElement, menuLink) {
     .getElementById('nav-dropdown-menu')
     .classList.toggle('hidden-dropdown-menu');
 }
+
+const projectData = [
+    {
+      name: 'Tonic',
+      description:
+        'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+      featuredImage: './images/tonic-work.png',
+      technologies: ['html', 'css', 'javascript'],
+      pointersTitle: 'CANOPY',
+      pointersLevel: 'Back End Dev',
+      pointersYear: '2015',
+      linkToLiveVersion: '#!',
+      linkToSource: '#!',
+    },
+    {
+      name: 'Multi-Post Stories',
+      description:
+        'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+      featuredImage: './images/multi-post-stories.png',
+      pointersTitle: 'CANOPY',
+      pointersLevel: 'Back End Dev',
+      pointersYear: '2015',
+      technologies: ['html', 'css', 'javascript'],
+      linkToLiveVersion: '#!',
+      linkToSource: '#!',
+    },
+    {
+      name: 'Tonic',
+      description:
+        'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+      featuredImage: './images/tonic-work2.png',
+      pointersTitle: 'CANOPY',
+      pointersLevel: 'Back End Dev',
+      pointersYear: '2015',
+      technologies: ['html', 'css', 'javascript'],
+      linkToLiveVersion: '#!',
+      linkToSource: '#!',
+    },
+    {
+      name: 'Multi-Post Stories',
+      description:
+        'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+      featuredImage: './images/multi-post-stories2.png',
+      pointersTitle: 'CANOPY',
+      pointersLevel: 'Back End Dev',
+      pointersYear: '2015',
+      technologies: ['html', 'css', 'javascript'],
+      linkToLiveVersion: '#!',
+      linkToSource: '#!',
+    },
+  ];
