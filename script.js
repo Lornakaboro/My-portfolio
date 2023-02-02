f17eee1d48d22b45cd749c991778e706410116e2
Lornakaboro <lornakaboro@gmail.com>
2023-02-02 20:57:20 +0300

    Generate popup dynamically

--- script.js
+++ script.js
@@ -101,6 +101,11 @@ const popup = document.getElementById("mobile-popup");
 function closePopUp() {
   popup.classList.add("hide-mobile-popup");
 }
+// desktop
+const desktopPopUp = document.getElementById("desktop-popup");
+function closeDesktopPopUp() {
+  desktopPopUp.classList.add("hide-desktop-popup");
+}
 
 function getTechnologies(technologies) {
   let tech = "";
@@ -133,7 +138,7 @@ function generateProjectData(projectList, platform) {
                   <ul class='tags'>
                       ${technologies}
                   </ul>
-                  <a href='#!' class='project-link' onclick='openPopUp(${i})'>
+                  <a href='#!' class='project-link' onclick='openPopUp(${i}, "mobile")'>
                       See Project
                   </a>
               </div>
@@ -168,7 +173,7 @@ function generateProjectData(projectList, platform) {
                       <ul class='tags'>
                           ${tech}
                       </ul>
-                      <a href='#!' class='project-link' onclick='openDesktopPopUp(${i})' >
+                      <a href='#!' class='project-link' onclick='openPopUp(${i}, "desktop")' >
                        See Project
                       </a>
                   </div>
@@ -184,92 +189,87 @@ document.getElementById("mobile-works").innerHTML = generateProjectData(
   "mobile"
 );
 
-function openPopUp(index) {
-  const technologiesUsed = getTechnologies(projectData[index].technologies);
-  const data = `
-    <div class='work'>
-        <div class='mobile-popup-title'>
-            <h2 class='work-title'>${projectData[index].name}</h2>
-            <i class='fa-regular fa-x mobile-popup-x-icon' onclick='closePopUp()'></i>
+function openPopUp(index, screen) {
+  if (screen === "mobile") {
+    const technologiesUsed = getTechnologies(projectData[index].technologies);
+    const data = `
+      <div class='work'>
+          <div class='mobile-popup-title'>
+              <h2 class='work-title'>${projectData[index].name}</h2>
+              <i class='fa-regular fa-x mobile-popup-x-icon' onclick='closePopUp()'></i>
+          </div>
+          <div class="work-pointers">
+            <h3>CANOPY</h3>
+            <div class="work-pointers-bullets"></div>
+            <p>Back End Dev</p>
+            <div class="work-pointers-bullets"></div>
+            <p>2015</p>
+          </div>
+          <img src='${projectData[index].featuredImage}' class='work-image' alt='image of a lady doing yoga'>
+          <p class='work-description'>${projectData[index].description}</p>
+          <ul class='tags'>
+              ${technologiesUsed}
+          </ul>
+          <div class='mobile-popup-links'>
+              <a href='${projectData[index].linkToLiveVersion}' class='mobile-popup-project-link mobile-popup-project-link1'>
+                  See live
+                  <img src='./images/Popup-icon.png' alt='image of a small icon' class='popup-icon'>
+              </a>
+              <a href='${projectData[index].linkToSource}' class='mobile-popup-project-link'>
+                  See Source
+                  <img src='./images/github-vector.png' alt='github icon' class='popup-icon'>
+              </a>
+          </div>
+      </div>
+      `;
+    popup.innerHTML = data;
+    popup.classList.remove("hide-mobile-popup");
+  }
+  if (screen === "desktop") {
+    const currentProjectData = projectData[index];
+    const desktopTechnologies = getTechnologies(
+      projectData[index].desktopTechnologies
+    );
+    const data = `
+      <div class='desktop-popup' >
+          <div class='desktop-popup-title'>
+              <h2 class='work-title'>${currentProjectData.name}</h2>
+          <i class='fa-regular fa-x desktop-dropdown-x-icon' onclick='closeDesktopPopUp()'></i>
+          </div>
+          <div class="work-pointers">
+            <h3>${currentProjectData.desktopPointersTitle}</h3>
+            <div class="work-pointers-bullets"></div>
+            <p>${currentProjectData.desktopPointersLevel}</p>
+            <div class="work-pointers-bullets"></div>
+            <p>${currentProjectData.desktopPointersYear}</p>
         </div>
-        <div class="work-pointers">
-          <h3>CANOPY</h3>
-          <div class="work-pointers-bullets"></div>
-          <p>Back End Dev</p>
-          <div class="work-pointers-bullets"></div>
-          <p>2015</p>
-        </div>
-        <img src='${projectData[index].featuredImage}' class='work-image' alt='image of a lady doing yoga'>
-        <p class='work-description'>${projectData[index].description}</p>
-        <ul class='tags'>
-            ${technologiesUsed}
-        </ul>
-        <div class='mobile-popup-links'>
-            <a href='${projectData[index].linkToLiveVersion}' class='mobile-popup-project-link mobile-popup-project-link1'>
-                See live
-                <img src='./images/Popup-icon.png' alt='image of a small icon' class='popup-icon'>
-            </a>
-            <a href='${projectData[index].linkToSource}' class='mobile-popup-project-link'>
-                See Source
-                <img src='./images/github-vector.png' alt='github icon' class='popup-icon'>
-            </a>
-        </div>
-    </div>
-    `;
-  popup.innerHTML = data;
-  popup.classList.remove("hide-mobile-popup");
-}
-
-// desktop
-const desktopPopUp = document.getElementById("desktop-popup");
-function closeDesktopPopUp() {
-  desktopPopUp.classList.add("hide-desktop-popup");
+        <img src='${currentProjectData.desktopFeaturedImage}' class='desktop-popup-work-image' alt='image of a website hero-section'>
+          <div class='desktop-popup-description'>
+              <div class='desktop-popup-work-description'>
+                  <p>${currentProjectData.desktopDescription}</p>
+              </div>
+              <div class='desktop-popup-items'>
+                  <ul class=' desktop-popup-tags'>
+                  ${desktopTechnologies}
+                  </ul>
+                  <a href='${currentProjectData.linkToLiveVersion}' class=' desktop-popup-project-link'>
+                  See live
+                      <img src='./images/Popup-icon.png' alt='image of a small icon' class='popup-icon'>
+                  </a>
+                  <a href='${currentProjectData.linkToSource}' class=' desktop-popup-project-link'>
+                  See Source
+                      <img src='./images/github-vector.png' alt='github icon' class='popup-icon'>
+                  </a>
+              </div>
+          </div>
+      </div>
+      `;
+    desktopPopUp.innerHTML = data;
+    desktopPopUp.classList.remove("hide-desktop-popup");
+  }
 }
 
 document.querySelector("#desktop-works").innerHTML = generateProjectData(
   projectData,
   "desktop"
 );
-
-function openDesktopPopUp(counter) {
-  const currentProjectData = projectData[counter];
-  const desktopTechnologies = getTechnologies(
-    projectData[counter].desktopTechnologies
-  );
-  const data = `
-    <div class='desktop-popup' >
-        <div class='desktop-popup-title'>
-            <h2 class='work-title'>${currentProjectData.name}</h2>
-        <i class='fa-regular fa-x desktop-dropdown-x-icon' onclick='closeDesktopPopUp()'></i>
-        </div>
-        <div class="work-pointers">
-          <h3>${currentProjectData.desktopPointersTitle}</h3>
-          <div class="work-pointers-bullets"></div>
-          <p>${currentProjectData.desktopPointersLevel}</p>
-          <div class="work-pointers-bullets"></div>
-          <p>${currentProjectData.desktopPointersYear}</p>
-      </div>
-      <img src='${currentProjectData.desktopFeaturedImage}' class='desktop-popup-work-image' alt='image of a website hero-section'>
-        <div class='desktop-popup-description'>
-            <div class='desktop-popup-work-description'>
-                <p>${currentProjectData.desktopDescription}</p>
-            </div>
-            <div class='desktop-popup-items'>
-                <ul class=' desktop-popup-tags'>
-                ${desktopTechnologies}
-                </ul>
-                <a href='${currentProjectData.linkToLiveVersion}' class=' desktop-popup-project-link'>
-                See live
-                    <img src='./images/Popup-icon.png' alt='image of a small icon' class='popup-icon'>
-                </a>
-                <a href='${currentProjectData.linkToSource}' class=' desktop-popup-project-link'>
-                See Source
-                    <img src='./images/github-vector.png' alt='github icon' class='popup-icon'>
-                </a>
-            </div>
-        </div>
-    </div>
-    `;
-  desktopPopUp.innerHTML = data;
-  desktopPopUp.classList.remove("hide-desktop-popup");
-}
